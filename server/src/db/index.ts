29176b07bc58983ef20ad20cfb4df67037f51922
Grzegorz Łyczak <grzegorz.lyczak@microsoft.wsei.edu.pl>
2019-10-30 20:09:44 +0100

    add env varible

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,10 @@
 import { Sequelize } from 'sequelize-typescript';
 import path from 'path';
 
-const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
-  dialect: 'postgres',
-  repositoryMode: true,
-  models: [path.join(__dirname, '../modules/**/*.model.ts')],
+const sequelize = new Sequelize(process.env.DATA_BASE!, process.env.DATA_BASE_LOGIN!, process.env.DATA_BASE_PASSWORD!, {
+	dialect: 'postgres',
+	repositoryMode: true,
+	models: [ path.join(__dirname, '../modules/**/*.model.ts') ],
 });
 
 export default sequelize;
